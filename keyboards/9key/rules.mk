c43e42b944cd81a7681c12d7a8e87a9dd1161e3e
AABatteries <luk.filipski@gmail.com>
2021-09-28 14:42:55 +0200

    Update rules.mk
    enabled BOOTMAGIC and BACKLIGHT features

--- keyboards/9key/rules.mk
+++ keyboards/9key/rules.mk
@@ -8,13 +8,13 @@ BOOTLOADER = qmk-dfu
 #   change to "no" to disable the options, or define them in the Makefile in
 #   the appropriate keymap folder that will get included automatically
 #
-BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+BOOTMAGIC_ENABLE = yes       # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no        # Commands for debug and configuration
 NKRO_ENABLE = yes            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
+BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output
 UNICODE_ENABLE = yes         # Unicode
 RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
